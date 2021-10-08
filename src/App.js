327feea6c5722eb9be58eca32365cb1fd5b690b5
Michael <michael_parvis@hotmail.com>
2021-10-08 17:26:36 +0100

    conflict fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 
 import Header from "./components/header/Header";
-import Button from "./components/button/Button";
 import MobileBackgroundImage from "./components/mobile-background-image/MobileBackgroundImage";
 
 function App() {
