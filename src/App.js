e557298ae759fee63db35776f3ef0063de879baa
Michael <michael_parvis@hotmail.com>
2021-10-08 14:06:03 +0100

    added background of vaper, need to fix resize bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/header/Header";
+import MobileBackgroundImage from "./components/mobile-background-image/MobileBackgroundImage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MobileBackgroundImage />
     </div>
   );
 }
